68c81b42a0ab6fd7202e853c693994ef0e0aae3e
brian <fritz@jfmd.org>
2021-11-30 15:36:21 -0500

    remove all the inline iframe styling

--- category-videos.php
+++ category-videos.php
@@ -31,7 +31,7 @@ get_header(); ?>
                 </article>
 
 				<div class="">
-                <iframe src="https://player.vimeo.com/video/<?php the_field ('vimeo_embed'); ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+                <iframe src="https://player.vimeo.com/video/<?php the_field ('vimeo_embed'); ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
 				</div>	
 
 			<?php endwhile; ?>			
