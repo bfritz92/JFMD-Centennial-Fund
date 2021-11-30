8451e60781939ee65f573e1ebe5d91040a4c27cb
brian <fritz@jfmd.org>
2021-11-30 14:10:00 -0500

    Herb's advice

--- category-videos.php
+++ category-videos.php
@@ -8,8 +8,7 @@ get_header(); ?>
 <h1 class="entry-title">Centennial Stories</h1>
 <section class="">
 	<?php
-		$args = array(
-			'post_type'  	=> 'videos', 
+		$args = array( 
 			'order'			=> 'ASC',
 			'orderby'		=> 'rand',									
 			'posts_per_page'=> 1,
@@ -20,8 +19,8 @@ get_header(); ?>
 			while( $main_posts->have_posts() ) : $main_posts->the_post(); ?>
 				
 				<article>
-					<!--<p class="purple">Our Most Recent Video</p>-->
-					<!-- <a href="<?php the_permalink(); ?>" ><h2 class="yellow"><?php the_title(); ?></h2></a> -->
+					
+					<!-- <a href="<?php the_permalink(); ?>" ><h2 class=""><?php the_title(); ?></h2></a> -->
 					<div class="">
 						<!--<p class="purple"><?php the_field ('video_short_story'); ?></p>-->
 						<!-- <?php if (get_field('video_short_story')) : ?>-->
@@ -46,8 +45,8 @@ get_header(); ?>
 </section>
 
 <div class="">
-	<div class="cell">
-	<p class="purple border-bottom pb-1" style="max-width:500px;">Hear local young people and parents share their mental health related challenges and the steps that they took to heal. </p>
+	<div class="">
+	<p class="">Hear local young people and parents share their mental health related challenges and the steps that they took to heal. </p>
 	</div>
 </div>
 <!-- THE LØØP -->
@@ -55,8 +54,6 @@ get_header(); ?>
 	<?php
 		$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 		$args = array(
-   			'post_type'  	=> 'videos', 
-			'category_name'  => 'video',
 			'order'			=> 'DESC',
 			'orderby'		=> 'date', 
 			'posts_per_page'=> 21,
